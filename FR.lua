c4951bdb6507e880782c1c23afa6294ac6cc13d9
Beast007 <106586213+Beast007x@users.noreply.github.com>
2023-02-22 12:23:37 +0545

    Update FR.lua

--- FR.lua
+++ FR.lua
@@ -1,1 +1,29 @@
+--updated February 22/2023
+getgenv().Request = function(Username, Picture)
+    local BindableFunction  = Instance.new("BindableFunction", game.Workspace)
+    local Username          = Username or "Username"
+    local Picture           = Picture or "rbxassetid://1"
+    
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title           = Username,
+        Text            = "Sent you a friend request!",
+        Icon            = Picture,
+        Duration        = 5,
+        Callback        = BindableFunction,
+        Button1         = "Accept",
+        Button2         = "Decline"
+    })
 
+    BindableFunction.OnInvoke = function(Response)
+        if Response == "Accept" then
+            game:GetService("StarterGui"):SetCore("SendNotification", {
+                Title       = "New Friend",
+                Text        = Username,
+                Icon        = Picture,
+                Duration    = 5
+            }) 
+
+            BindableFunction:Destroy()
+        end
+    end 
+end
